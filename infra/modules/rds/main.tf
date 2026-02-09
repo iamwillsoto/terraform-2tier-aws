212fc2e86946ecea6fffdb83ca6f74ad743290b5
Will Soto <wsoto12@wgu.edu>
2026-02-08 19:09:58 -0500

    Fix RDS identifier to start with a letter

--- infra/modules/rds/main.tf
+++ infra/modules/rds/main.tf
@@ -8,7 +8,7 @@ resource "aws_db_subnet_group" "this" {
 }
 
 resource "aws_db_instance" "this" {
-  identifier             = "${var.name_prefix}-mysql"
+  identifier = "db-${var.name_prefix}-mysql"
   engine                 = "mysql"
   engine_version         = "8.0"
   instance_class         = "db.t3.micro"
