d7550eab16eef778e03f860a1eb8dbc7d33db08a
Will Soto <wsoto12@wgu.edu>
2026-02-08 20:32:08 -0500

    ALB health check: allow 200-399 for WordPress redirects

--- infra/modules/alb/main.tf
+++ infra/modules/alb/main.tf
@@ -22,7 +22,7 @@ resource "aws_lb_target_group" "this" {
     unhealthy_threshold = 2
     timeout             = 5
     interval            = 30
-    matcher             = "200"
+    matcher             = "200-399"
   }
 }
 
