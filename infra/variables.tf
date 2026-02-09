ce1e48222c71b36b1197282bc18a335a2734ce68
Will Soto <wsoto12@wgu.edu>
2026-02-08 19:07:25 -0500

    Declare DB variables for RDS module

--- infra/variables.tf
+++ infra/variables.tf
@@ -7,3 +7,16 @@ variable "vpc_cidr" {
   type    = string
   default = "10.0.0.0/16"
 }
+
+variable "db_name" {
+  type = string
+}
+
+variable "db_username" {
+  type = string
+}
+
+variable "db_password" {
+  type      = string
+  sensitive = true
+}
